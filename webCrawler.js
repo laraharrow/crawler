7ab48efe54e6bb62919abf29463e0a7c52f89069
laraismael1 <laraismael123@gmail.com>
2018-02-20 13:41:26 -0800

    basic installed

--- webCrawler.js
+++ webCrawler.js
@@ -1,2 +1,8 @@
+// used to make HPTTP requests
 var request = require('request');
-var cheerio = require('cheerioo');
+// use to parse and select HTML elements on a page
+var cheerio = require('cheerio');
+// use to parse URLs
+var URL = require('url-parse');
+
+
